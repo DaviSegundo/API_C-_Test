c9b0a9ba3406c56555b0d32d06f3a31b0e33c22f
Davi S.P <davisp2009@hotmail.com>
2021-11-05 01:20:40 -0300

    Initial Commit

---
+++ API_Filmes_CS/Controllers/FilmeController.cs
@@ -0,0 +1,44 @@
+﻿using API_Filmes_CS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Filmes_CS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+
+            return CreatedAtAction(nameof(RecuperarFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet ("{id}")]
+        public IActionResult RecuperarFilmesPorId(int id)
+        {
+            Filme filme =  filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            else return NotFound();
+        }
+    }
+}
---
+++ API_Filmes_CS/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Filmes_CS.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "O campo título é obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duração deve estar entre 1 e 600 minutos")]
+        public string Duracao { get; set; }
+        
+    }
+}
